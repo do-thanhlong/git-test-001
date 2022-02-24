5547155779ac60fd5cd0cdad6acfda13ba44e42f
Do Thanh Long <longdt@doxanh.dev>
2022-02-24 18:45:23 +0700

    test -007

---
+++ src/com/longdt/LocalMain.java
@@ -0,0 +1,9 @@
+package com.longdt;
+
+public class LocalMain {
+    public static void main (String[] args) {
+
+        System.out.println("hello,git");
+        System.out.println(" test keep Local Main");
+    }
+}
